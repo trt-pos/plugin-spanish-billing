9689d227277ec296c6abaf18fd1d4dc698cd96c4
Borja Castellano <borjacastellano1@gmail.com>
2025-05-20 22:01:30 +0200

    improved some ui components

--- src/main/java/org/lebastudios/theroundtable/pluginspanishbilling/PluginSpanishBilling.java
+++ src/main/java/org/lebastudios/theroundtable/pluginspanishbilling/PluginSpanishBilling.java
@@ -53,7 +53,7 @@ public class PluginSpanishBilling implements IPlugin
     {
         if (!AppInstallation.thisInstalation().isMaster()) return List.of();
         
-        final var billingSettings = new LabeledIconButton("Facturación", new IconView("billing.png"), _ ->
+        final var billingSettings = new LabeledIconButton("Facturación", "billing.png", _ ->
         {
             MainStageController.getInstance().setCentralNode(new BillManagementPaneController());
         });
